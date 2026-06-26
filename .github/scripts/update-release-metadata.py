9a60758e284cab1bff3256a71a4cbb6ea83cb59b
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 00:29:48 +0200

    Test updater with json

--- .github/scripts/update-release-metadata.py
+++ .github/scripts/update-release-metadata.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import argparse
+import json
 from pathlib import Path
-Path('foo.txt').write_text('hi')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('version')
+args = parser.parse_args()
+path = Path('.zenodo.json')
+data = json.loads(path.read_text(encoding='utf-8'))
+data['version'] = args.version
+path.write_text(json.dumps(data, indent=2) + '\n', encoding='utf-8')
