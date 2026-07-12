47aa3c7d9bff8e6bb52e1083a39a5b036ba91f21
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-12 10:05:27 +0200

    Remove temporary benchmark marker

--- app/src/jmh/java/de/regelsuche/benchmark/ExpressionIdentityBenchmarkMarker.java
+++
@@ -1,7 +0,0 @@
-package de.regelsuche.benchmark;
-
-/** Keeps the ADR #242 benchmark suite explicitly discoverable in pull-request diffs. */
-final class ExpressionIdentityBenchmarkMarker {
-    private ExpressionIdentityBenchmarkMarker() {
-    }
-}
