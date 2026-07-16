a0823ae97128bb21476f3691476ffa18799c2dd3
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-16 21:03:41 +0200

    Return atomic solver executions

--- regelsuche-solver-ir/src/main/java/de/regelsuche/solver/ir/SolverBackend.java
+++ regelsuche-solver-ir/src/main/java/de/regelsuche/solver/ir/SolverBackend.java
@@ -2,11 +2,10 @@ package de.regelsuche.solver.ir;
 
 import de.regelsuche.solver.ir.SolverIr.BackendDescriptor;
 import de.regelsuche.solver.ir.SolverIr.Obligation;
-import de.regelsuche.solver.ir.SolverIr.SolverResult;
 
-/** Executes one canonical obligation without changing its semantics. */
+/** Translates and executes one canonical obligation as one hash-linked unit. */
 public interface SolverBackend {
     BackendDescriptor descriptor();
 
-    SolverResult solve(Obligation obligation);
+    SolverExecution execute(Obligation obligation);
 }
