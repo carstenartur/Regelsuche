4f46184d6a44ab8caf18f143a1033f1492134275
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-09 09:50:16 +0200

    test: retain protocol cross-field invariants for scheduler v2

--- regelsuche-learning/src/test/java/de/regelsuche/evolution/EvolutionRewriteProgramPopulationExecutionProtocolTest.java
+++ regelsuche-learning/src/test/java/de/regelsuche/evolution/EvolutionRewriteProgramPopulationExecutionProtocolTest.java
@@ -63,6 +63,41 @@ class EvolutionRewriteProgramPopulationExecutionProtocolTest {
     }
 
     @Test
+    void explicitFactoryRejectsMixedSchedulingAndSemanticVersions() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> EvolutionRewriteProgramPopulationExecutionProtocol.create(
+                EvolutionRewriteProgramPopulationEngine.class,
+                PopulationEngineSemanticsVersion.LEGACY_POPULATION_ENGINE_V1,
+                DeterministicRewriteProgramMutator.class,
+                MutatorSemanticsVersion.ROTATED_PREFIX_MUTATOR_V1,
+                ProposalOrderingPolicy
+                    .KEY_ASCENDING_THEN_GLOBAL_SEED_ROTATION_V1,
+                OffspringSchedulingPolicy.STRATIFIED_MUTATION_KIND_V1,
+                2,
+                MutationSeedDerivationPolicy
+                    .STUDY_HASH_GENERATION_PARENT_HASH_SHA256_PREFIX64_V1,
+                SurvivorSelectionPolicy
+                    .FITNESS_DESC_NODES_ASC_HASH_ASC_UNIQUE_ALPHA_ELITES_V1));
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> EvolutionRewriteProgramPopulationExecutionProtocol.create(
+                EvolutionRewriteProgramPopulationEngine.class,
+                PopulationEngineSemanticsVersion
+                    .PROTOCOL_DRIVEN_POPULATION_ENGINE_V2,
+                DeterministicRewriteProgramMutator.class,
+                MutatorSemanticsVersion.STRATIFIED_MUTATION_KIND_MUTATOR_V2,
+                ProposalOrderingPolicy
+                    .KEY_ASCENDING_THEN_GLOBAL_SEED_ROTATION_V1,
+                OffspringSchedulingPolicy.ROTATED_PREFIX_V1,
+                2,
+                MutationSeedDerivationPolicy
+                    .STUDY_HASH_GENERATION_PARENT_HASH_SHA256_PREFIX64_V1,
+                SurvivorSelectionPolicy
+                    .FITNESS_DESC_NODES_ASC_HASH_ASC_UNIQUE_ALPHA_ELITES_V1));
+    }
+
+    @Test
     void factoriesRejectInvalidPolicyInputsBeforeHashing() {
         assertThrows(
             NullPointerException.class,
