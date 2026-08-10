939f3e891271e06800bcd9dd4108c30790d722a7
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-10 23:37:00 +0200

    Extend Maven build contract to phase two modules

--- maven-build-contract/src/test/java/de/regelsuche/build/MavenBuildContractTest.java
+++ maven-build-contract/src/test/java/de/regelsuche/build/MavenBuildContractTest.java
@@ -36,6 +36,13 @@ class MavenBuildContractTest {
         "regelsuche-search",
         "regelsuche-validation",
         "regelsuche-math-algorithms",
+        "regelsuche-math-jas",
+        "regelsuche-persistence",
+        "regelsuche-solver-ir",
+        "regelsuche-learning",
+        "regelsuche-discovery",
+        "regelsuche-experiments",
+        "regelsuche-cli",
         "maven-build-contract"
     );
 
@@ -84,7 +91,7 @@ class MavenBuildContractTest {
     );
 
     @Test
-    void reactorContainsTheDeclaredJavaCoreSlice() throws Exception {
+    void reactorContainsTheDeclaredJavaModuleSlice() throws Exception {
         Path root = repositoryRoot();
         Document parent = parse(root.resolve("pom.xml"));
 
